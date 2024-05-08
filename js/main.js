5f022fbf635ad39d389b6d67b323541a901fb2d8
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-08 23:59:09 +0530

    5. addEventListener

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@
 const view2 = document.querySelector("#view2");
 const div = view2.querySelector("div");
 const h2 = div.querySelector("h2");
-console.log(h2);
 
+// syntax = addEventListener(event, function, useCapture);
 
-
-
+const doSomething = () => {
+    alert("doing something!");
+}
+// we need add eventListener wherever we are interacting in this case it h2
+h2.addEventListener("click", doSomething, false);
+// fasle is a default useCapture;
 
 
 
