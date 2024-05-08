76294f6798323d0a87cb3d0a24bc819b899d6d2a
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-08 23:52:05 +0530

    4. selecting nodes or elements according to DOM tree navigation

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// eventListeners 
+
+const view2 = document.querySelector("#view2");
+const div = view2.querySelector("div");
+const h2 = div.querySelector("h2");
+console.log(h2);
+
+
+
+
+
+
+
+
+
+
+
+
+
