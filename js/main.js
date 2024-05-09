c2e4295ed37ca693fce35dab33ebfbad08e6b091
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-10 01:18:44 +0530

    7. initApp( ) and some stupid mistakes😡😡😡😡💩

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // eventListeners 
-
+/*
 const view2 = document.querySelector("#view2");
 const div = view2.querySelector("div");
 const h2 = div.querySelector("h2");
-
+*/
 // syntax = addEventListener(event, function, useCapture);
 /*
 const doSomething = () => {
@@ -29,14 +29,31 @@ h2.addEventListener("click", (event) => {
 // few more examples
 // let's look into an event example which is also makes it safer to start working with document object model
 
-document.addEventListener("readystatechange", (e) => {
-    if (e.target.readyState === "complete") {
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
         console.log("readyState: complete");
-    }
-    // also we can initiate app method initApp()
-    initApp();
-})
+        initApp();
+        // also we can initiate app method initApp() to load all our other events
+    } 
+});
 
+const initApp = () => {
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
+
+    view.addEventListener("click", (event) => {
+        view.style.backgroundColor = "purple";
+    });
+    
+    div.addEventListener("click", (event) => {
+        div.style.backgroundColor = "green";
+    });
+
+    h2.addEventListener("click", (event) => {
+        event.target.textContent = "clicked";
+    })
+}
 
 
 
